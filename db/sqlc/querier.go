c5350bf16005dcd8124cd7d8611a49fc60f2ea6c
Vincent Low <vincent.low037@gmail.com>
2023-02-14 23:12:32 +0700

    add CRUD func for Entries & Transfers entity

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -10,10 +10,20 @@ import (
 
 type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
+	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error)
 	DeleteAccount(ctx context.Context, id int64) error
+	DeleteEntry(ctx context.Context, id int64) error
+	DeleteTransfer(ctx context.Context, id int64) error
 	GetAccount(ctx context.Context, id int64) (Account, error)
+	GetEntry(ctx context.Context, id int64) (Entry, error)
+	GetTransfer(ctx context.Context, id int64) (Transfer, error)
 	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
+	ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error)
+	ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
+	UpdateEntry(ctx context.Context, arg UpdateEntryParams) (Entry, error)
+	UpdateTransfer(ctx context.Context, arg UpdateTransferParams) (Transfer, error)
 }
 
 var _ Querier = (*Queries)(nil)

9d6b75d8de1eb392d84f967a04b025114d839e9c
Vincent Low <vincent.low037@gmail.com>
2023-02-15 20:42:11 +0700

    add config & env variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"github.com/VL-037/go-bank/util"
 	"log"
 	"os"
 	"testing"
@@ -9,17 +10,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/go_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}

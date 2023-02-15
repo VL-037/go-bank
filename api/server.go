73fe7344fd320b4c45d58b5148dabeb066e3298a
Vincent Low <vincent.low037@gmail.com>
2023-02-15 22:59:29 +0700

    add mock db for Account

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Server serves HTTP requests for the banking service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // NewServer for routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
